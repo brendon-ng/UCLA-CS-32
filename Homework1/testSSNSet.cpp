f2778841ec5ee62b48e39f2aef31a2652a9a1a22
brendon-ng <brendonn8@gmail.com>
2019-01-19 18:08:09 -0800

    Removed some comments

--- Homework1/testSSNSet.cpp
+++ Homework1/testSSNSet.cpp
@@ -1,40 +1,41 @@
-////
-////  testSSNSet.cpp
-////  Homework1
-////
-////  Created by Brendon Ng on 1/19/19.
-////  Copyright © 2019 Brendon Ng. All rights reserved.
 //
+//  testSSNSet.cpp
+//  Homework1
 //
-//#include "SSNSet.h"
-//#include <iostream>
-//#include <string>
-//#include <cassert>
-//using namespace std;
-//
-//int main()
-//{
-//    SSNSet s1;
-//    s1.print();
-//    s1.add(533526224);
-//    assert(s1.size() == 1);
-//    assert(s1.add(672928471)== true);
-//    assert(s1.add(134450149) == true);
-//    assert(s1.add(492304194) == true);
-//    assert(s1.add(672928471)== false);
-//    assert(s1.size() == 4);
-//    s1.print();
-//    
-//    SSNSet s2 = s1;
-//    assert(s2.size() == 4);
-//    s2.print();
-//    
-//    SSNSet s3;
-//    s3.add(535115501);
-//    
-//    s3 = s2;
-//    assert(s2.size() == 4);
-//    s2.print();
-//    
-//}
-//
+//  Created by Brendon Ng on 1/19/19.
+//  Copyright © 2019 Brendon Ng. All rights reserved.
+
+
+#include "SSNSet.h"
+#include <iostream>
+#include <string>
+#include <cassert>
+using namespace std;
+
+int main()
+{
+    SSNSet s1;
+    s1.print();
+    s1.add(533526224);
+    assert(s1.size() == 1);
+    assert(s1.add(672928471)== true);
+    assert(s1.add(134450149) == true);
+    assert(s1.add(492304194) == true);
+    assert(s1.add(672928471)== false);
+    assert(s1.size() == 4);
+    s1.print();
+    
+    SSNSet s2 = s1;
+    assert(s2.size() == 4);
+    s2.print();
+    
+    SSNSet s3;
+    s3.add(535115501);
+    
+    s3 = s2;
+    assert(s2.size() == 4);
+    s2.print();
+    
+}
+
+
