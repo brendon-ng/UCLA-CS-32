a01ac9971da0ad34d969e9aefdb27040e0e6f38a
brendon-ng <brendonn8@gmail.com>
2019-03-05 18:40:08 -0800

    sorts

--- Homework4/sorts.cpp
+++ Homework4/sorts.cpp
@@ -148,6 +148,7 @@ void insertion_sort(vector<Store>& s, bool comp(const Store&, const Store&))
         for(int j=i; j>0 ; j--){
             if(comp(s[j],s[j-1])){
                 swap(s[j], s[j-1]);
+                break;
             }
         }
     }
